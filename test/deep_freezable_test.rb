a94cb28fb9b602c70fbea11d18ffefc48b4ca3da
jizya0208 <moridyan606@gmail.com>
2021-08-26 13:40:40 +0000

    First Commit

---
+++ test/deep_freezable_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require './lib/bank'
+require './lib/team'
+
+class DeepFreezableTest < Minitest::Test # クラス外で定数を参照する::
+  def test_deep_freeze_to_array
+    assert_equal ['Japan', 'US', 'India'], Team::COUNTRIES
+    assert Team::COUNTRIES.frozen?
+    assert Team::COUNTRIES.all? { |country| country.frozen? }
+  end
+end
